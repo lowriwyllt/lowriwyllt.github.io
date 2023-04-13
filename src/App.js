463c80687f8c13d0afd0c3449ea7682e06c8c7fa
lowriwyllt <lowriwyllt@live.co.uk>
2023-04-13 13:49:36 +0100

    add the main pages I will have on my page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import { useState } from "react";
 import "./App.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Main/Home";
+import CV from "./components/Main/CV";
+import Travel from "./components/Main/Travel";
+import { Route, Routes } from "react-router";
 
 function App() {
   const [activeNavbar, setActiveNavbar] = useState(false);
+  let appClass = "App";
+  if (activeNavbar) {
+    appClass += " AppNav";
+  }
+
   return (
     <div className="App">
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cv" element={<CV />} />
         <Route path="/travel" element={<Travel />} />
-        <Route path="/games" element={<Games />} />
-      </Routes>*/}
+        {/* <Route path="/games" element={<Games />} /> */}
+      </Routes>
       <Navbar activeNavbar={activeNavbar} setActiveNavbar={setActiveNavbar} />
       <Footer />
     </div>
